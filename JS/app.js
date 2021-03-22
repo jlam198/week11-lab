796386bc7727096cc159303c8b285771e8cc90a3
Jeremy <jlam198@my.bcit.ca>
2021-03-22 10:22:18 -0700

    Added for loop to add todo item to completedTodos if status in todoList is complete

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,13 @@
 // your JavaScript goes here, you can delete this comment
 const todoList = [{todo: "finish lab 11", status: "started"}, 
-                {todo: "finish readings", status: "completed"},
+                {todo: "finish readings", status: "complete"},
                 {todo: "clean room", status: "started"},
-                {todo: "eat breakfast", status: "completed"}]
+                {todo: "eat breakfast", status: "complete"}]
 
 const completedTodos = []
+
+todoList.forEach(function(element) {
+    if (element.status === "complete") {
+        completedTodos.push(element)
+    }
+})
