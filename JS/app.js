78e7fbe5e2e5616aba922dbf1c618ecfbe851ca1
Jeremy <jlam198@my.bcit.ca>
2021-03-22 09:15:00 -0700

    Created todoList and completedTodos arrays in JS file

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,7 @@
 // your JavaScript goes here, you can delete this comment
+const todoList = [{todo: "finish lab 11", status: "started"}, 
+                {todo: "finish readings", status: "completed"},
+                {todo: "clean room", status: "started"},
+                {todo: "eat breakfast", status: "completed"}]
+
+const completedTodos = []
